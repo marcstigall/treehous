d87ea5cfa67488df280f00cae15aca7791eef4fc
Marcus <marcstigall@gmail.com>
2017-11-14 19:23:47 -0500

    added more responsiveness

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
     $(".btn1").click(function(){
         $("img").hide();
-    });
+    }); //hides monkeys on the page
       $(".btn2").click(function(){
         $("img").show();
-    });
+    }); // shows monkeys on the page
 });
